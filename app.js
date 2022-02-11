981eb2697be096c0aa0c382675cd54c4aedd69e9
Anshika Gautam <anshikagautam130@gmail.com>
2022-02-11 22:04:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const app = express();
