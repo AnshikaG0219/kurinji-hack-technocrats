44571c9b66602a22162f295a619299dfe025abaf
Anshika Gautam <anshikagautam130@gmail.com>
2022-02-11 22:11:30 +0530

    node setup

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
